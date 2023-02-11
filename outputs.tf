fa4042bf73213a5c231f617b5a98bcdbccdd7d34
Almir <almirosmanovic.eposta@gmail.com>
2023-02-11 23:05:04 +0100

    Add instance ID as output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "public_ip" {
   value = ["${aws_instance.example.*.public_ip}"]
   description = "The public IP of the web server"
 }
+
+output "instance_id" {
+  value = ["${aws_instance.example.*.id}"]
+  description = "The instance ID of the web server
