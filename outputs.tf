b05977cbc54bb0b4ccf945365cbb02944cd1d56c
Almir <almirosmanovic.eposta@gmail.com>
2023-02-12 01:48:56 +0100

    Add instance count as Output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "instance_id" {
   value = ["${aws_instance.example.*.id}"]
   description = "The instance ID of the web server"
 }
+
+output "instance_count" {
+  value = var.instance_count
+  description = "The number of instances created"
+}
+  
